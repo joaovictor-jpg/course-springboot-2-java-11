36f1531a4f8ce163af9053122e61e2333be0be00
joao victor <joaovictormdasilva676@gmail.com>
2022-08-07 11:48:11 -0300

    Product Insert

--- src/main/java/com/educandoweb/course/resources/ProductResource.java
+++ src/main/java/com/educandoweb/course/resources/ProductResource.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,19 +17,25 @@ import com.educandoweb.course.services.ProductService;
 @RestController
 @RequestMapping(value = "/products")
 public class ProductResource {
-	
+
 	@Autowired
 	private ProductService service;
-	
+
 	@GetMapping
 	public ResponseEntity<List<Product>> findAll() {
 		List<Product> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
-	
+
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Product> findById(@PathVariable Long id) {
 		Product obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+	@PostMapping()
+	public ResponseEntity<Product> insert(@RequestBody Product obj) {
+		obj = service.insert(obj);
 		return ResponseEntity.ok().body(obj);
 	}
 
--- src/main/java/com/educandoweb/course/services/ProductService.java
+++ src/main/java/com/educandoweb/course/services/ProductService.java
@@ -6,22 +6,34 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.educandoweb.course.entities.Category;
 import com.educandoweb.course.entities.Product;
 import com.educandoweb.course.repositories.ProductRepository;
 
 @Service
 public class ProductService {
-	
+
 	@Autowired
 	private ProductRepository repository;
-	
+	@Autowired
+	private CategoryService service;
+
 	public List<Product> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public Product findById(Long id) {
 		Optional<Product> obj = repository.findById(id);
 		return obj.get();
 	}
 
+	public Product insert(Product obj) {
+		Long id = obj.getId();
+		Product entity = new Product(null, obj.getName(), obj.getDescription(), obj.getPrice(), obj.getImgUrl());
+		Category category = service.findById(id);
+
+		entity.getCategories().add(category);
+
+		return repository.save(entity);
+	}
 }
